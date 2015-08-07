2cf9cf61f93c497485916b85d2fe67b2e96c31fe
Marcus Dunn <daremyth@gmail.com>
2015-08-07 15:59:56 -0700

    More plays and bug fixes, persistence

--- public/clock.js
+++ public/clock.js
@@ -28,85 +28,188 @@ var b_live_ball_fouls = [
 "Defensive tackle B73 is flagged for holding.  "
 ]
 
-var a_live_ball_fouls_play_ending = [
-"QB A22 throws an illegal forward pass to A9 who is tackled short of the line to gain.  ",
-"QB A22 throws an illegal forward pass to A9 who is tackled beyond of the line to gain.  "
+//Runoff: 0 - none, 1 - B's option, 2 - A's option
+//Foul: 0 - none, 1 - A foul, 2 - B foul
+//Start: 0 - Ready, 1 - Snap, 2 - Running
+var endingPlays = [
+	{text: "QB A22 throws an illegal forward pass to A9 who is tackled short of the line to gain.  " ,
+	 runoff: 0,
+	 clock: 25,
+	 foul: 1,
+	 start: 0},
+	 {text: "QB A22 throws an illegal forward pass to A9 who is tackled beyond of the line to gain.  ",
+	 runoff: 0,
+	 clock: 25,
+	 foul: 1,
+	 start: 0},
+	 {text: "As he is about to be tackled near the sideline, ball carrier A22 throws the ball backward and out of bounds to stop the clock.  ",
+	 runoff: 1,
+	 clock: 25,
+	 foul: 1,
+	 start: 0},
+	 {text: "QB A22 throws an illegal forward pass which falls incomplete beyond the neutral zone.  ",
+	 runoff: 1,
+	 clock: 25,
+	 foul: 1,
+	 start: 1},
+	 {text: "QB A22 throws an illegal forward pass which falls incomplete behind the neutral zone.  ",
+	 runoff: 1,
+	 clock: 25,
+	 foul: 1,
+	 start: 1},
+	 {text: "A22 is tackled by the facemask short of the line to gain.  ",
+	 runoff: 0,
+	 clock: 25,
+	 foul: 2,
+	 start: 0},
+	 {text: "A22 is tackled by the facemask beyond the line to gain.  ",
+	 runoff: 0,
+	 clock: 25,
+	 foul: 2,
+	 start: 0},
+	 {text: "The ball carrier is tackled inbounds short of the line to gain.  ",
+	 runoff: 0,
+	 clock: 40,
+	 foul: 0,
+	 start: 2},
+	 {text: "The ball carrier is tackled inbounds short of the line to gain.  ",
+	 runoff: 0,
+	 clock: 40,
+	 foul: 0,
+	 start: 2},
+	 {text: "The ball carrier is tackled inbounds beyond the line to gain.  ",
+	 runoff: 0,
+	 clock: 40,
+	 foul: 0,
+	 start: 0},
+	 {text: "The ball carrier is tackled inbounds beyond the line to gain.  ",
+	 runoff: 0,
+	 clock: 40,
+	 foul: 0,
+	 start: 0},
+	 {text: "The quarterback's forward pass is incomplete.  ",
+	 runoff: 0,
+	 clock: 40,
+	 foul: 0,
+	 start: 1},
+	 {text: "The quarterback's forward pass is incomplete.  ",
+	 runoff: 0,
+	 clock: 40,
+	 foul: 0,
+	 start: 1},
+	 {text: "Team A's punt is short and is recovered behind the neutral zone by A34 who is immediately tackled.  ",
+	 runoff: 0,
+	 clock: 25,
+	 foul: 0,
+	 start: 1},
+	 {text: "A2's pass is intercepted at the 50 by B54, who runs for a short gain then is tackled.  ",
+	 runoff: 0,
+	 clock: 25,
+	 foul: 0,
+	 start: 1}
+
+
 ]
 
-var a_live_ball_fouls_play_ending_time = [
-"As he is about to be tackled near the sideline, ball carrier A22 throws the ball backward and out of bounds to stop the clock.  ",
-"QB A22 throws an illegal forward pass which falls incomplete beyond the neutral zone.  ",
-"QB A22 throws an illegal forward pass which falls incomplete behind the neutral zone.  "
-];
-
-var b_live_ball_fouls_play_ending = [
-"A22 is tackled by the facemask short of the line to gain.  ",
-"A22 is tackled by the facemask beyond the line to gain.  "
+// 0 - A, 1 - B, 2 - Both
+var helmet_plays = [
+	{text: "During the play A66's helmet comes off.  "  ,
+	 team: 0},
+	 	{text: "During the play, A55 loses his helmet.  ",
+	 team: 0},
+	 	{text: "During the play A33's helmet comes off.  ",
+	 team: 0},
+	 	{text: "During the play B35's helmet comes off.  ",
+	 team: 1},
+	 	{text: "During the play helmets come off A77 and B53.  ",
+	 team: 2}
 ]
 
-var play_ends_clock_runs = [
-"The ball carrier is tackled inbounds short of the line to gain.  "
-];
-
-var play_ends_clock_stops = [
-"The ball carrier is tackled inbounds beyond the line to gain.  ",
-"The quarterback's forward pass is incomplete.  "
-];
-
-var play_ends_clock_stops_25 = [
-"Team A's punt is short and is recovered behind the neutral zone by A34 who is immediately tackled.  ",
-"A2's pass is intercepted at the 50 by B54, who runs for a short gain then is tackled.  "
+var injury_plays = [
+	{text: "During the play A82 is injured.  " ,
+	 team: 0},
+	 {text: "During the play, A55 is injured.  "  ,
+	 team: 0},
+	 {text: "During the play B75 is injured.  " ,
+	 team: 1},
+	 {text: "During the play, B78 is injured.  "  ,
+	 team: 1},
+	 {text: "During the play both A66 and B39 go down with injuries.  "  ,
+	 team: 2}
 ]
 
-var a_helmet_off = [
-"During the play A66's helmet comes off.  ",
-"During the play, A55 loses his helmet.  ",
-"During the play A33's helmet comes off.  "
-];
+var runner = getParameterByName("runner")
+var runCount = 0
 
-var b_helmet_off = [
-"During the play B35's helmet comes off.  ",
-"During the play, B77 loses his helmet.  "
-];
-
-var a_and_b_helmet_off = [
-"During the play helmets come off A77 and B53.  "
+var CFOPlays = [
+[0,-1,-1,-1,-1,-1],
+[-1,0,-1,-1,-1,-1],
+[1,-1,-1,-1,-1,-1],
+[-1,-1,0,-1,-1,-1,8],
+[-1,-1,-1,0,-1,-1,8],
+[2,-1,-1,-1,-1,-1],
+[-1,-1,-1,0,-1,-1,2],
+[-1,-1,-1,-1,-1,-1,2,0],
+[-1,-1,-1,-1,-1,-1,2,3,-1],
+[-1,-1,-1,-1,-1,-1,2,-1,0],
+[-1,-1,-1,-1,-1,-1,2,-1,2],
+[-1,-1,-1,-1,-1,-1,8,1,-1],
+[-1,-1,-1,-1,0,-1,8,1,-1],
+[-1,-1,-1,1,-1,-1,8,1,-1],
+[-1,-1,-1,-1,-1,-1,9,1,-1],
+[-1,-1,-1,-1,-1,-1,9,3,-1],
+[-1,-1,-1,-1,-1,-1,8,4,-1],
+[-1,-1,-1,-1,-1,-1,8,-1,1],
+[-1,-1,-1,-1,-1,-1,8,-1,3],
+[-1,-1,-1,-1,-1,-1,11,-1,1],
+[-1,-1,-1,-1,-1,-1,11,-1,2],
+[-1,-1,-1,-1,-1,-1,8,-1,4],
+[-1,-1,-1,-1,-1,-1,8,0,0],
+[-1,-1,-1,-1,-1,-1,8,0,3],
+[-1,-1,-1,-1,-1,-1,8,3,3],
+[-1,-1,-1,-1,-1,-1,8,3,0],
+[-1,-1,-1,-1,-1,-1,10,3,3]
 ]
 
-var a_injury = [
-"During the play A82 is injured.  ", 
-"During the play, A55 is injured.  "
-];
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
 
-var b_injury = [
-"During the play B75 is injured.  ",
-"During the play, B78 is injured.  "
-];
+function randElement(array, place) {
+	place = typeof place !== 'undefined' ? place : -1;
+	pick = Math.floor(Math.random() * array.length)
+	pick_debug[place] = pick
+	return array[pick]
+}
 
-var a_and_b_injury = [
-"During the play both A66 and B39 go down with injuries.  "
-]
-
-function randElement(array) {
-	return array[Math.floor(Math.random() * array.length)];
+function randElementMaybe(array, chance, place) {
+	if(Math.random() > chance/100) { 
+		place = typeof place !== 'undefined' ? place : -1;
+		pick_debug[place] = -1
+		return null 
+	}
+	pick = Math.floor(Math.random() * array.length)
+	pick_debug[place] = pick
+	return array[pick]
 }
 
 var prob_debug = []
-function setDebug(place) {
-	prob_debug[place] = true
-}
+var pick_debug = [-1, -1, -1, -1, -1, -1]
 
 function test(place) {
-	var play_probs = [80, 10, 10, 10, 10, 10, 0, 0, 0, 0, 20, 20, 20, 20, 20, 20]
+	var play_probs = [10, 10, 10, 10, 10, 10, 0, 0, 0, 0, 20, 20, 20, 20, 20, 20]
 	if(Math.random() < play_probs[place]/100) {
-		setDebug(place)
 		return true
 	}
+	pick_debug[place] = -1
 	return false
 }
 
 function oneOfFourMaybe(place) {
-	var play_probs = [80, 80]
+	var play_probs = [40, 20]
 	if(Math.random() < play_probs[place]/100) {
 		return pick(0,3)
 	}
@@ -115,78 +218,152 @@ function oneOfFourMaybe(place) {
 
 function pick(min, max) {
 	var pick = Math.floor(Math.random() * (max - min + 1)) + min;
-	setDebug(pick)
 	return pick;
 }
 
-var all = [a_clock_stopping_fouls, b_clock_stopping_fouls, a_live_ball_fouls_at_snap, b_live_ball_fouls_at_snap, a_live_ball_fouls, b_live_ball_fouls, 
-/*6*/ a_live_ball_fouls_play_ending, a_live_ball_fouls_play_ending_time, b_live_ball_fouls_play_ending, play_ends_clock_runs, play_ends_clock_stops, play_ends_clock_stops_25, 
-/*12*/ a_helmet_off, b_helmet_off, a_and_b_helmet_off, a_injury, b_injury, a_and_b_injury];
+var all = [a_clock_stopping_fouls, b_clock_stopping_fouls, a_live_ball_fouls_at_snap, b_live_ball_fouls_at_snap, a_live_ball_fouls, b_live_ball_fouls];
 
 var a_foul
-	var b_foul
-	var helmets_off // 0 - none, 1 - A only, 2 - B only, 3 - Both; BITMASK FTW
-	var injuries
-	var runoff_option // 1 - B runoff option, 2 - A runoff option, 3- None
-	var dead
-	var forty_clock
+var b_foul
+var helmets_off // 0 - none, 1 - A only, 2 - B only, 3 - Both
+var injuries
+var runoff_option // 1 - B runoff option, 2 - A runoff option, 3- None
+var dead
+var forty_clock
+var ready_snap 
 
 function generatePlay() {
+	var param = getParameterByName("play")
+	if(param != "") {
+		var storedPlays = param.replace(/\s+/g, '').split(',');
+		for(var i=0; i<storedPlays.length; i++) { storedPlays[i] = +storedPlays[i]; } 
+		console.log(storedPlays)
+	} else {
+		storedPlays = []
+	} 
+	if(runner=="true") {
+		storedPlays = CFOPlays[runCount]
+		runCount++
+		if(runCount>=27) { runCount = 0 }
+	}
 	a_foul=false
 	b_foul=false
-	helmets_off = 0 // 0 - none, 1 - A only, 2 - B only, 3 - Both; BITMASK FTW
+	helmets_off = 0 // 0 - none, 1 - A only, 2 - B only, 3 - Both
 	injuries = 0
 	runoff_option = 0 // 1 - B runoff option, 2 - A runoff option, 3- None
 	dead=false
 	forty_clock=false
+	ready_snap = 0 //0 - ready, 1 - snap, 2 - running
+	pick_debug = [-1, -1, -1, -1, -1, -1]
 	play = ""
-	console.log("Preplay")
-	for (i=0; i<6; i++) {
-		if(test(i)) {
-			console.log("Play index - " + i)
-			play+=randElement(all[i]);
-			if(i==0 || i==1) {
-				i==0 ? a_foul=true : b_foul=true
-				i==0 ? runoff_option=1 : runoff_option=2
-				dead=true
-				break
+	if(storedPlays.length>0) {
+		for (i=0; i<6; i++) {
+			if(storedPlays[i]>-1) {
+				play+=all[i][storedPlays[i]]
+				if(i==0 || i==1) {
+					i==0 ? a_foul=true : b_foul=true
+					i==0 ? runoff_option=1 : runoff_option=2
+					dead=true
+					break
+				}
+				if(i==0 || i==2 || i==4 ) {
+					a_foul = true
+				}
+				if(i==1 || i==3 || i==5) {
+					b_foul = true
+				}
 			}
-			if(i==0 || i==2 || i==4 ) {
-				a_foul = true
-			}
-			if(i==1 || i==3 || i==5) {
-				b_foul = true
+		}
+	} else {
+		for (i=0; i<6; i++) {
+			if(test(i)) {
+				play+=randElement(all[i], i);
+				if(i==0 || i==1) {
+					i==0 ? a_foul=true : b_foul=true
+					i==0 ? runoff_option=1 : runoff_option=2
+					dead=true
+					break
+				}
+				if(i==0 || i==2 || i==4 ) {
+					a_foul = true
+				}
+				if(i==1 || i==3 || i==5) {
+					b_foul = true
+				}
 			}
 		}
 	}
 	console.log("Dead - " + dead)
 	if(!dead) {
-		var ending_play = pick(6,11)
-		if(ending_play == 6 || ending_play == 7 ) { a_foul = true; runoff_option=1 }
-		if(ending_play == 8) { b_foul = true }
-		if(ending_play == 9 || ending_play==10) { forty_clock = true }
-		console.log("Ending play - " + ending_play)
-		play+=randElement(all[ending_play])
-		helmet_play = oneOfFourMaybe(0)
-		if(helmet_play>0) {
-			helmets_off = helmet_play;
-			if(ending_play!=10 && ending_play!=11) { runoff_option = helmet_play; }
-			if(helmet_play==2) { forty_clock = true } else { forty_clock = false }
-			play+=randElement(all[helmet_play+11])
+		var ending_play, helmet_play, injury_play
+		if(storedPlays.length>0) {
+			ending_play = endingPlays[storedPlays[6]]
+			if(storedPlays[7] > -1) helmet_play = helmet_plays[storedPlays[7]]
+			if(storedPlays[8] > -1) injury_play = injury_plays[storedPlays[8]]
+		} else {
+			ending_play = randElement(endingPlays, 6)
+			helmet_play = randElementMaybe(helmet_plays, 30, 7)
+			injury_play = randElementMaybe(injury_plays, 30, 8)
 		}
-		console.log(helmet_play)
-		injury_play = oneOfFourMaybe(0)
-		if(injury_play>0) {
-			injuries = injury_play;
-			if(ending_play!=10 && ending_play!=11) { runoff_option = injury_play; }
-			if(injury_play==2) { forty_clock = true } else { if(helmet_play!=2) { forty_clock = false }}
-			play+=randElement(all[injury_play+14])
-		}
-		console.log(injury_play)
-		//Opposing actions = no runoff
-		if(injury_play + helmet_play == 3 || injury_play == 3 || helmet_play == 3) { runoff_option = false }
-		if(a_foul || b_foul) { runoff_option = false }
+		play+=ending_play.text
+		a_foul = a_foul || (ending_play.foul == 1)
+		b_foul = b_foul || (ending_play.foul == 2)
+		runoff_option = ending_play.runoff
+		if(a_foul && b_foul) { runoff_option = 0 }
+		//Forty clock only set if there are no fouls
+		forty_clock = (ending_play.clock==40 && !a_foul && !b_foul)
+		ready_snap = (a_foul || b_foul) ? 0 : ending_play.start
+		
+		if(helmet_play || injury_play) {
+			if(helmet_play) {
+				helmets_off = helmet_play;
+				play+=helmets_off.text
+			} 
+			if(injury_play) {
+				injuries = injury_play;
+				play+=injuries.text;
+			}
+
+			/* Calculating the state of the 40 clock */
+			if((injury_play != null && injury_play.team > 0) || (helmet_play != null && helmet_play.team > 0)) {
+				forty_clock = true
+			} else {
+				forty_clock = false
+			}
+			
+			if(ready_snap == 2) { 
+				ready_snap = 0 
+				//No runoff (yet) and no offsetting fouls
+				if(runoff_option == 0 && (!a_foul && !b_foul)) {
+					if(injury_play != null && helmet_play != null && injury_play.team != helmet_play.team) {
+						runoff_option = 0
+					} else if((injury_play != null && injury_play.team == 0) || (helmet_play != null && helmet_play.team == 0)) {
+						runoff_option = 1
+					} else if((injury_play != null && injury_play.team == 1) || (helmet_play != null && helmet_play.team == 1)) {
+						runoff_option = 2
+					}
+				}
+			}
+
+			/* Calculate the runoff state.  Complicated due to offesetting fouls/injuries/hemlets off */
+			//No runoff for offseting helmet/injuries
+			if((injury_play != null && injury_play.team == 3) || (helmet_play != null && helmet_play.team == 3)) {
+				runoff_option = 0
+			}
+			//Runoff option from foul, and offsetting injury/helmet
+			else if(
+				((injury_play != null && injury_play.team == 1) || (helmet_play != null && helmet_play.team == 1)) && (a_foul && ending_play.runoff==1) ||
+				((injury_play != null && injury_play.team == 2) || (helmet_play != null && helmet_play.team == 2) && runoff_option == 2)
+			  ) {
+				runoff_option = 0
+			}
+
+			
+
+		}		
 	}
+
+	console.log(pick_debug)
 	
 
 
@@ -198,7 +375,9 @@ function generatePlay() {
 		"<br>Helmets off: " + (helmets_off==1 ? "A" : helmets_off==2 ? "B" : helmets_off==3 ? "Both" : "None") +
 		"<br>Injuries: " + (injuries==1 ? "A" : injuries==2 ? "B" : injuries==3 ? "Both" : "Neither") +
 		"<br>Runoff option: " + (runoff_option==1 ? "B's choice" : runoff_option==2 ? "A's choice" : runoff_option==3 ? "Neither" : "Neither") +
-		"<br>Play clock: " + (forty_clock ? "40" : "25")
+		"<br>Play clock: " + (forty_clock ? "40" : "25") +
+		"<br>Clock starts on: " + (ready_snap==0 ? "Ready" : ready_snap==1 ? "Snap" : "Running") +
+		"<br>Debug info: " + pick_debug
 	document.getElementById("demo").innerHTML = situation_text; 
 	document.getElementById("debug").hidden = true;
 	document.getElementById("debug").innerHTML = debug_text; 
@@ -215,6 +394,9 @@ function resetButtons() {
 	document.getElementById("bRunoff").checked = false
 	document.getElementById("25clock").checked = false
 	document.getElementById("40clock").checked = false
+	document.getElementById("readyClock").checked = false
+	document.getElementById("snapClock").checked = false
+	document.getElementById("runningClock").checked = false
 }
 
 function gradeAnswers() {
@@ -232,7 +414,7 @@ function gradeAnswers() {
 		document.getElementById("answer").innerHTML = wrongHTML
 		return;	
 	}
-	if(runoff_option == 2 && document.getElementById("aRunoff").checked) {
+	if(runoff_option == 2 && !document.getElementById("aRunoff").checked) {
 		document.getElementById("answer").innerHTML = wrongHTML
 		return;	
 	}
@@ -241,6 +423,18 @@ function gradeAnswers() {
 		return;	
 	}
 	if(forty_clock != document.getElementById("40clock").checked) {
+		document.getElementById("answer").innerHTML = wrongHTML
+		return;	
+	}
+	if(ready_snap == 0 && !document.getElementById("readyClock").checked) {
+		document.getElementById("answer").innerHTML = wrongHTML
+		return;	
+	}
+	if(ready_snap == 1 && !document.getElementById("snapClock").checked) {
+		document.getElementById("answer").innerHTML = wrongHTML
+		return;	
+	}
+	if(ready_snap == 2 && !document.getElementById("runningClock").checked) {
 		document.getElementById("answer").innerHTML = wrongHTML
 		return;	
 	}
